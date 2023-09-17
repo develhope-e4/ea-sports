12bea18625b71b77187afb2199efaa883cec6425
Nico Casiean <casiean2@gmail.com>
2023-09-17 20:14:31 +0200

    Mejorando componente

--- src/components/CardDestacada/CardDestacada.js
+++ src/components/CardDestacada/CardDestacada.js
@@ -1,22 +1,13 @@
 import "./CardDestacada.scss";
-/**
- *
- 
-@param {string} texto - Texto que va a llevar el boton
-@param {Boolean} esPrimario - Boolean para indicar si el boton es primario o secundario
-@param {Boolean} isLight - Boolean para indicar si el boton es Dark o Light
-@param {Callback} onClick - Callback que se ejecuta cada vez que el usuario hace click en el boton
-*
-@returns Botón dorado si es secundario y botón blanco con borde negro si es primario
-*/
 
-const CardDestacada = ({ backgroundImage, gameLogo }) => {
+
+const CardDestacada = ({backgroundImage,  url, gameLogo }) => {
   const cardStyle = {
     backgroundImage: `url(${backgroundImage})`,
   };
 
   return (
-    <a href="https://www.ea.com/es-es" className="linkCardDestacada">
+    <a href={url} className="linkCardDestacada">
       <div className="backgroundImage" style={cardStyle}>
         <div className="cardDestacada">
           <img
