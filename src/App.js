401032ab244ee08d0610a2c63305eb62385b6f78
Manuel <manu.benitez1@hotmail.com>
2023-09-18 16:38:34 +0200

    refactor: componente icono

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ import Tabs from "./components/Tabs/Tabs";
 import cardData from "./data/CardData.mock";
 
 import Icono from "./components/Icono/Icono";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { AiOutlineMenu } from "react-icons/ai";
+import IconoEjemplo from "./assets/iconos/IconoEjemplo";
 function App() {
   const [activeTab, setActiveTab] = useState("NoticiasEA");
 
@@ -152,18 +155,9 @@ function App() {
             onClick={() => console.log("click en el boton blanco")}
             texto={"Únete ya"}
           />
-          <Icono
-            xmlns={"http://www.w3.org/2000/svg"}
-            width={"23px"}
-            height={"23px"}
-            viewBox={"0 0 23 23"}
-            version={"1.1"}
-            style={{}}
-            esRedSocial={false}
-            d={
-              "M 11.5 14.375 C 9.914062 14.375 8.625 13.085938 8.625 11.5 C 8.625 9.914062 9.914062 8.625 11.5 8.625 C 13.085938 8.625 14.375 9.914062 14.375 11.5 C 14.375 13.085938 13.085938 14.375 11.5 14.375 Z M 14.375 4.3125 C 14.375 2.726562 13.085938 1.4375 11.5 1.4375 C 9.914062 1.4375 8.625 2.726562 8.625 4.3125 C 8.625 5.898438 9.914062 7.1875 11.5 7.1875 C 13.085938 7.1875 14.375 5.898438 14.375 4.3125 Z M 14.375 18.6875 C 14.375 17.101562 13.085938 15.8125 11.5 15.8125 C 9.914062 15.8125 8.625 17.101562 8.625 18.6875 C 8.625 20.273438 9.914062 21.5625 11.5 21.5625 C 13.085938 21.5625 14.375 20.273438 14.375 18.6875 Z M 14.375 18.6875 "
-            }
-          />
+          <Icono icono={<BsThreeDotsVertical style={{ fill: "white" }} />} />
+          <Icono icono={<AiOutlineMenu style={{ fill: "white" }} />} />
+          <Icono icono={<IconoEjemplo />} />
         </div>
         <Boton
           esPrimario={true}
