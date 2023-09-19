357785ae4a57475eb68a86bc0c84cd0147e045e3
Jeniffer Flores <hachikofu03@gmail.com>
2023-09-19 19:02:58 +0200

    feat:EaPlayIcon

--- src/Subscription/Subscription.js
+++ src/Subscription/Subscription.js
@@ -1,12 +1,22 @@
 import Boton from "../components/Boton/Boton";
 const Subscription = () => {
-    return(
-     <Boton
-            esPrimario={false}
-            isLight={false}
-            onClick={() => console.log("click en el boton dorado")}
-            texto={"Comprar ahora"}
-          />
-    );
-}
+  return (
+    <div>
+      <div>
+        <Boton
+          esPrimario={false}
+          isLight={true}
+          onClick={() => console.log("click en el boton blanco")}
+          texto={"Únete ya"}
+        />
+      </div>
+      <Boton
+        esPrimario={false}
+        isLight={false}
+        onClick={() => console.log("click en el boton negro")}
+        texto={"Más información"}
+      />
+    </div>
+  );
+};
 export default Subscription;
