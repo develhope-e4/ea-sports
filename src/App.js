76ceeb4b9b9ed4a8dbba8d9d641eecd84b54a0d3
Wilmer Salazar <wilmersalazarlara@gmail.com>
2023-09-19 16:35:42 +0200

    refactor: styles

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React, { useState } from "react";
 import "./App.css";
 import "./styles/index.scss";
-<<<<<<< HEAD
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Development from "./pages/development/Development";
 import Home from "./pages/home/Home";
 
-=======
-import EjemploComponente from "./components/EjemploComponente/EjemploComponente";
-import Boton from "./components/Boton/Boton";
-import CardDestacada from "./components/CardDestacada/CardDestacada";
-import HighlightGroup from "./components/Highlight-Group/Highlight-Group";
->>>>>>> dc0d00c (feat: Subiendo seccion de cards destacadas agrupadas)
 function App() {
   const [activeTab, setActiveTab] = useState("NoticiasEA");
 
@@ -30,7 +23,6 @@ function App() {
   };
 
   return (
-<<<<<<< HEAD
     <BrowserRouter>
       {/* Aca va el elemento NAVBAR */}
       <Routes>
@@ -39,81 +31,6 @@ function App() {
       </Routes>
       {/* Aca va el elemento FOOTER */}
     </BrowserRouter>
-=======
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div className="flex-col-center">
-          {/**
-           * Manera de invocar a mi componente reutilizable
-           */}
-          <EjemploComponente
-            esPrimario={true}
-            texto={"Boton primario"}
-            onClick={() => console.log("click en el boton primario")}
-          />
-          <EjemploComponente
-            esPrimario={false}
-            texto={"Boton secundario"}
-            onClick={() => console.log("click en el boton secundario")}
-          />
-          <EjemploComponente
-            esPrimario={false}
-            disabled={true}
-            onClick={() => console.log("click en el boton disabled")}
-            texto={"Boton secundario"}
-          />
-          <Boton
-            esPrimario={true}
-            isLight={false}
-            onClick={() => console.log("click en el boton dorado")}
-            texto={"Comprar ahora"}
-            />
-            <Boton
-            esPrimario={false}
-            isLight={false}
-            onClick={() => console.log("click en el boton negro")}
-            texto={"Últimos juegos"}
-            />
-            <Boton
-            esPrimario={false}
-            isLight={true}
-            onClick={() => console.log("click en el boton blanco")}
-            texto={"Únete ya"}
-            />
-
-
-            
-
-            <CardDestacada
-            url="http://google.com"
-            backgroundImage="./highlight/1/bg.png"
-            gameLogo="./highlight/1/logo.svg"
-            />
-
-            <HighlightGroup/>
-        
-
-         
-           
-        </div>
-
-      
-
-      </header>
-    </div>
->>>>>>> dc0d00c (feat: Subiendo seccion de cards destacadas agrupadas)
   );
 }
 
---
+++ src/components/Cards/cardData.js
@@ -0,0 +1,332 @@
+// cardData.js
+import "./Cards.scss";
+
+const cardData = {
+  NoticiasEA: [
+    {
+      imagenSrc: "Cards-section/NOTICIASEA/Card-ea-positive.png",
+      nombre: "Electronic Arts Inc.",
+      fecha: "10-ago-2023",
+      titulo:
+        "Ya están abiertas las votaciones para el primer Premio al juego positivo de...",
+      texto:
+        "El Premio al juego positivo de EA reconoce a finalistas de eSports que han elevado la cultura del juego y celebra su contribución con positividad, habilidad e impacto en la comunidad de la ALGS.",
+    },
+    {
+      imagenSrc: "Cards-section/NOTICIASEA/Card-ea-evento.jpg",
+      nombre: "Electronic Arts Inc.",
+      fecha: "27-jul-2023",
+      titulo: " Celebramos el futuro de EA con nuestro personal en prácticas!",
+      texto:
+        "Nuestros eventos de prácticas reunen a personal en prácticas para conectar, colaborar, aprender e iniciar sus carreras de EA.",
+    },
+    {
+      imagenSrc: "Cards-section/NOTICIASEA/Card-ea-cliff.png",
+      nombre: "Electronic Arts Inc.",
+      fecha: "10-jul-2023",
+      titulo: "Descubre Cliffhanger Games™ ",
+      texto:
+        "Descubre Cliffhanger Games™, un nuevo estudio de EA que desarrolla un juego de acción y aventuras para un jugador, basado en el personaje de Pantera Negra de Marvel.",
+    },
+    {
+      imagenSrc: "Cards-section/NOTICIASEA/Card-ea-positive.png",
+      nombre: "Electronic Arts Inc.",
+      fecha: "05-jul-2023",
+      titulo: "Celebramos que los juegos saquen los mejor de las personas ",
+      texto:
+        "El Premio al juego positivo de EA reconoce a las personas que compiten Esports que hayan tenido un impacto significativo a la hora de crear una cultura fuerte, positiva y sana.",
+    },
+    {
+      imagenSrc: "Cards-section/NOTICIASEA/Card-ea-img.sala.png",
+      nombre: "Electronic Arts Inc.",
+      fecha: "28-jun-2023",
+      titulo:
+        "Project Rene: Pruebas de juego de la nueva generación de Los Sims ",
+      texto:
+        "Descubre como el equipo de los Maxis detrás de la evolución de Los Sims está involucrando a la comunidad en el proceso del desarrollo.",
+    },
+    {
+      imagenSrc: "Cards-section/NOTICIASEA/card-pride.jpg",
+      nombre: "Electronic Arts Inc.",
+      fecha: "14-jun-2023",
+      titulo: "Homenaje a la comunidad LGBTQ+",
+      texto:
+        "En EA nos enorgullece homenajear a la comunidad LGBTQ+ y unirnos a este Mes del Orgullo.",
+    },
+  ],
+  EASPORTSFC: [
+    {
+      imagenSrc: "Cards-section/EASPORTSFC/sport-ea2.png",
+      nombre: "16-ago-2023",
+      fecha: "",
+      titulo:
+        "EA SPORTS FT™ 24 | Notas a pie de campo - Análisis detallado de Clubes",
+      texto:
+        "Este es el año de nuevos comienzos con EA SPORTS FC™ 24 y por eso Clubes tendrá la mayor renovación de los últimos tiempos.",
+    },
+    {
+      imagenSrc: "Cards-section/EASPORTSFC/sport-ea2.png",
+      nombre: "08-ago-2023",
+      fecha: "",
+      titulo:
+        "EA SPORTS FT™ 24 | Notas a pie de campo - Análisis detallado de Ultimate Te...",
+      texto:
+        "EA SPORTS FC™ 24 Football Ultimate Team incorpora Evoluciones, jugadoras y otras actualizaciones solicitadas por la comunidad.",
+    },
+    {
+      imagenSrc: "Cards-section/EASPORTSFC/sport-ea2.png",
+      nombre: "04-ago-2023",
+      fecha: "",
+      titulo:
+        "EA SPORTS FT™ 24 | Notas a pie de campo - Presentamos EA SPORTS FC Pro",
+      texto: "",
+    },
+    {
+      imagenSrc: "Cards-section/EASPORTSFC/sport-ea2.png",
+      nombre: "31-jul-2023",
+      fecha: "",
+      titulo:
+        "EA SPORTS FT™ 24 | Notas a pie de campo - Análisis detallado (modo Carrera) ",
+      texto:
+        "Da forma a tu club como mánager o elige exactamente cómo juegas en el campo como futbolista en la nueva carrera de EA SPORTS FC ™ 24",
+    },
+    {
+      imagenSrc: "Cards-section/EASPORTSFC/sport-ea2.png",
+      nombre: "18-jul-2023",
+      fecha: "",
+      titulo:
+        "EA SPORTS FT™ 24 | Notas a pie de campo - Análisis detallado de jugabilidad ",
+      texto:
+        "Tu expeciencia en EA SPORTS FT™ 24 se verá mejorada gracias a los muchos cambios que se han aplicado sobre el terreno de juego, como HyperMotionV o PlayStyles optimizado por Opta, entre otros.",
+    },
+    {
+      imagenSrc: "Cards-section/EASPORTSFC/sport-ea1.jpg",
+      nombre: "13-jul-2023",
+      fecha: "",
+      titulo: "EA SPORTS FT™ 24 | Novedades sobre el Cross-Play",
+      texto: "Novedades sobre el Cross-Play.",
+    },
+  ],
+  StarWars: [
+    {
+      imagenSrc: "Cards-section/STARWARS/sept-update.jpg",
+      nombre: "Star Wars ™  Galaxy of Heroes",
+      fecha: "25-ago-2023",
+      titulo: "Vista previa del calendario de septiembre",
+      texto: "¡Echa un vistazo a los próximos eventos!",
+    },
+    {
+      imagenSrc: "Cards-section/STARWARS/kit-reveal-user.png",
+      nombre: "Star Wars ™  Galaxy of Heroes",
+      fecha: "08-ago-2023",
+      titulo: "Revelación de kit - Soldado exploradora",
+      texto: "¡Echa un vistazo a las habilidades de la soldado exploradora en esta revelación de kit!",
+    },
+    {
+      imagenSrc: "Cards-section/STARWARS/august-update.jpg",
+      nombre: "Star Wars ™  Galaxy of Heroes",
+      fecha: "25-jul-2023",
+      titulo: "Vista previa del calendario de agosto",
+      texto: "¡Echa un vistazo a los próximos eventos!",
+    },
+    {
+      imagenSrc: "Cards-section/STARWARS/kit-reveal-nave.png",
+      nombre: "Star Wars ™  Galaxy of Heroes",
+      fecha: "18-jul-2023",
+      titulo: "Revelación de kit - Leviatán",
+      texto: "¡Echa un vistazon a las habilidades de Leviatán en esta revelación de kit!",
+    },
+    {
+      imagenSrc: "Cards-section/STARWARS/kit-reveal-osito.png",
+      nombre: "Star Wars ™  Galaxy of Heroes",
+      fecha: "13-jul-2023",
+      titulo: "Revelación de kit - Princesa Kneesaa",
+      texto: "¡Echa un vistazo a las habilidades de la princesa Kneesaa en esta revelación de kit!",
+    },
+    {
+      imagenSrc: "Cards-section/STARWARS/kit-reveal-osito.png",
+      nombre: "Star Wars ™  Galaxy of Heroes",
+      fecha: "13-jul-2023",
+      titulo: "Revelación de kit - Princesa Kneesaa",
+      texto: "",
+    },
+  ],
+  ApexLegends: [
+    {
+      imagenSrc: "Cards-section/APEX/1.jpg",
+      nombre: "Apex Legends",
+      fecha: "17-ago-2023",
+      titulo: "Campeonato de 3.er año: Todo lo que necesitas saber",
+      texto: "Consulta la informacion oficial del torneo de Campeonato de la ALGS del 6 al 10 de septiembre de 2023 en Birmingham (Reino Unido).",
+    },
+    {
+      imagenSrc: "Cards-section/APEX/2.png",
+      nombre: "Apex Legends",
+      fecha: "11-ago-2023",
+      titulo: "Apex Legends™: preguntas sobre el desarrollo de la historia (08/2023)",
+      texto: "Ponte al día con la sesión de preguntas sobre la historia con el equipo de narrativa en r/ApexLegends, del 1 de agosto de 2023.",
+    },
+    {
+      imagenSrc: "Cards-section/APEX/3.jpg",
+      nombre: "Apex Legends",
+      fecha: "07-ago-2023",
+      titulo: "Notas del parche de Apex Legends™: Resurreción",
+      texto: "Preparate para Resurreción: detalles sobre Revenant renacido, novedades del anillo, Clasificatorias y más en nuestras notas del parche.",
+    },
+    {
+      imagenSrc: "Cards-section/APEX/4.png",
+      nombre: "Apex Legends",
+      fecha: "04-ago-2023",
+      titulo: "Apex Legends™: preguntas sobre el desarrollo de Clasificatorias (07/2023)",
+      texto: "Ponte al día con la sesión de preguntas sobre Clasificatorias al equipo de desarrollo en r/ApexLegends, el 21 de julio de 2023.",
+    },
+    {
+      imagenSrc: "Cards-section/APEX/5.jpg",
+      nombre: "Apex Legends",
+      fecha: "03-ago-2023",
+      titulo: "Novedades que llegas con Apex Legends™: Resurrección",
+      texto: "Echa un vistazo a las novedades que llegan en Apex Legends™: Resurrección, con Revenant renacido, cambios del rifle de cargas ¡y mucho más!",
+    },
+    {
+      imagenSrc: "Cards-section/APEX/6.jpg",
+      nombre: "Apex Legends",
+      fecha: "20-jul-2023",
+      titulo: "Deslumbra con el evento de colección Red neón",
+      texto: "lumina las sombras con una luz de neón, como profesional del asesinato con destreza tecnológica en el Evento de colección Red neón.",
+    },
+  ],
+  LosSims4: [
+    {
+      imagenSrc: "Cards-section/SIMS/1-3-6.jpg",
+      nombre: "Los Sims 4",
+      fecha: "03-ago-2023",
+      titulo: "Actualización: 02/08/2023",
+      texto: "Llenando comederos y limpiando establos",
+    },
+    {
+      imagenSrc: "Cards-section/SIMS/2.jpg",
+      nombre: "Los Sims 4",
+      fecha: "20-jul-2023",
+      titulo: "Chestnut Ridge: construcciones destacadas en Los Sims™ 4 Rancho de Caballos",
+      texto: "Tú haces Los Sims con Chrissie YT, Simproved, Ninnster y Doctor Ashley.",
+    },
+    {
+      imagenSrc: "Cards-section/SIMS/1-3-6.jpg",
+      nombre: "Los Sims 4",
+      fecha: "18-jul-2023",
+      titulo: "Actualización: 18/07/2023",
+      texto: "Al galope tendido",
+    },
+    {
+      imagenSrc: "Cards-section/SIMS/4.png",
+      nombre: "Los Sims 4",
+      fecha: "14-jul-2023",
+      titulo: "Problemas que se están investigando: 14 de julio de 2023",
+      texto: "Problemas que se están invetigando.",
+    },
+    {
+      imagenSrc: "Cards-section/SIMS/5.png",
+      nombre: "Los Sims 4",
+      fecha: "22-jun-2023",
+      titulo: "Prepara tu montura en Los Sims 4 Rancho De Caballos Pack de Expansión",
+      texto: "Invita a Sims de ranchos vecinos y demos la bienvenida a los caballos.",
+    },
+    {
+      imagenSrc: "Cards-section/SIMS/1-3-6.jpg",
+      nombre: "Los Sims 4",
+      fecha: "13-jun-2023",
+      titulo: "Actualización: 13/06/2023",
+      texto: "Correcciones rápidas pero importantes",
+    },
+  ],
+  F1: [
+    {
+      imagenSrc: "Cards-section/F1/1.jpg",
+      nombre: "F1® 23",
+      fecha: "29-ago-2023",
+      titulo: "NOTAS DEL PARCHE DE F1® 23 (v1.10)",
+      texto: "Con este parche de F1® 23 llegan grandes añadidos y un australiano favorito de la afición.",
+    },
+    {
+      imagenSrc: "Cards-section/F1/2.png",
+      nombre: "F1® 23",
+      fecha: "15-ago-2023",
+      titulo: "ACTUALIZACIÓN DE LAS VALORACIONES DE PILOTOS DE F1® 23 (AGOSTO)",
+      texto: "Las vacaciones de verano han llegado y con ellas la actualización de las valoraciones de pilotos de F1® 23",
+    },
+    {
+      imagenSrc: "Cards-section/F1/3.jpg",
+      nombre: "F1® 23",
+      fecha: "07-ago-2023",
+      titulo: "NOTAS DEL PARCHE DE F1® 23 (v1.09)",
+      texto: "Llegan unas cuantas correcciones de errores y mejoras en la dificultad de F1® World.",
+    },
+    {
+      imagenSrc: "Cards-section/F1/4.jpg",
+      nombre: "F1® 23",
+      fecha: "24-jul-2023",
+      titulo: "NOTAS DEL PARCHE DE F1® 23 (v1.08)",
+      texto: "Mejoras en sesiones de clasificación y solución de problemas con asignación de neumáticos en carrera de esprint (Carrera profesional 2J).",
+    },
+    {
+      imagenSrc: "Cards-section/F1/5.jpg",
+      nombre: "F1® 23",
+      fecha: "10-jul-2023",
+      titulo: "NOTAS DEL PARCHE DE F1® 23 (v1.07)",
+      texto: "Incluye arreglos para los volantes Logitech y para un error que afectaba a las negociaciones de contrato en Carrera profesional.",
+    },
+    {
+      imagenSrc: "Cards-section/F1/6.jpg",
+      nombre: "F1® 23",
+      fecha: "29-jun-2023",
+      titulo: "COMPITE Y GANA RECOMPENSAS EN EL JUEGO DE F1® WORLD EN JULIO",
+      texto: "Algunas colaboraciones del mundo real y un nuevo tipo de evento llegan a F1® World en F1® 23.",
+    },
+  ],
+  Battlefield: [
+    {
+      imagenSrc: "Cards-section/BATTLEF/1.png",
+      nombre: "Battlefield 2042",
+      fecha: "25-ago-2023",
+      titulo: "ACTUALIZACIÓN N.° 5.3.0 DE BATTLEFIELD 2042",
+      texto: "Presentamos un nuevo sistema del códice, revisiones de los recuentos de vehículos, mejoras en el equilibrado de vehículos y más.",
+    },
+    {
+      imagenSrc: "Cards-section/BATTLEF/2.jpg",
+      nombre: "Battlefield 2042",
+      fecha: "15-ago-2023",
+      titulo: "Información de Battlefield - Actualización de desarrollo - Agosto de 2023",
+      texto: "Recompensas de Redux y modos tiempo limitado.",
+    },
+    {
+      imagenSrc: "Cards-section/BATTLEF/3.jpg",
+      nombre: "Battlefield 2042",
+      fecha: "11-ago-2023",
+      titulo: "ACTUALIZACIÓN N.°5.2.1 DE BATTLEFIELD 2042",
+      texto: "Pequeña actualización de calidad de vida para Battlefield 2042",
+    },
+    {
+      imagenSrc: "Cards-section/BATTLEF/4.jpg",
+      nombre: "Battlefield 2042",
+      fecha: "28-jul-2023",
+      titulo: "ACTUALIZACIÓN N. ° 5.2.0 DE BATTLEFIELD 2042",
+      texto: "Rediseño de Reloj de arena, nuevos accesorios de armas del arcón, órdenes de patrulla y más.",
+    },
+    {
+      imagenSrc: "Cards-section/BATTLEF/5.jpg",
+      nombre: "Battlefield 2042",
+      fecha: "06-jul-2023",
+      titulo: "Información de Battlefield - La directriz Arkangel",
+      texto: "Evento Directriz Arkangel y modo de juego Control por tiempo limitado.",
+    },
+    {
+      imagenSrc: "Cards-section/BATTLEF/6.jpg",
+      nombre: "Battlefield 2042",
+      fecha: "30-jun-2023",
+      titulo: "ACTUALIZACIÓN N.° 5.1.0 DE BATTLEFIELD 2042",
+      texto: "Mejores de calidad de vida tras el lanzamiento de la temporada 5: Nuevo amanecer.",
+    },
+  ],
+};
+
+export default cardData;
