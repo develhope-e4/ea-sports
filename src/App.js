725a69494ba2473cadd28f7ac5d43705f9647dfd
Manuel <manu.benitez1@hotmail.com>
2023-09-25 16:35:50 +0200

    feat: configuracion basica del router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import "./styles/index.scss";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -6,22 +6,6 @@ import Development from "./pages/development/Development";
 import Home from "./pages/home/Home";
 
 function App() {
-  const [activeTab, setActiveTab] = useState("NoticiasEA");
-
-  const handleTabClick = (tab) => {
-    setActiveTab(tab);
-  };
-
-  const tabNames = {
-    NoticiasEA: "Noticias De EA",
-    EASPORTSFC: "EA SPORTS FC",
-    StarWars: "Star Wars",
-    ApexLegends: "Apex Legends",
-    LosSims4: "Los Sims 4",
-    F1: "F1",
-    Battlefield: "Battlefield",
-  };
-
   return (
     <BrowserRouter>
       {/* Aca va el elemento NAVBAR */}
