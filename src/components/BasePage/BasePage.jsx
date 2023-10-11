35cf36e51ed12893de7c58319d756eb19ec06bdd
Wilmer Salazar <wilmersalazarlara@gmail.com>
2023-10-11 20:41:30 +0200

    fix: eliminando import react e instalando classnames para merge

--- src/components/BasePage/BasePage.jsx
+++ src/components/BasePage/BasePage.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 
 const BasePage = ({ children }) => {
   return <div className="base-page">{children}</div>;
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import "./Section.scss";
 import classNames from "classnames";
 
