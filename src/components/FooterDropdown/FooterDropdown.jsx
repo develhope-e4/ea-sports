326859449fee5f27932c92864a7090fc1d7183f8
Wilmer Salazar <wilmersalazarlara@gmail.com>
2023-10-11 16:37:43 +0200

    fix: index.html y vite.config.js

--- src/components/FooterDropdown/FooterDropdown.jsx
+++ src/components/FooterDropdown/FooterDropdown.jsx
@@ -7,7 +7,7 @@ const FooterDropdown = ({ items = [], dropdownTitle }) => {
   const activatorRef = useRef(null);
   const dropdownListRef = useRef(null);
   const [isOpen, setIsOpen] = useState(false);
-  const [selectedItem, setSelectedItem] = useState(items[0] || null); // Elemento seleccionado inicialmente
+  const [selectedItem, setSelectedItem] = useState(items[0] || null); 
 
   const clickHandler = () => {
     setIsOpen(!isOpen);
@@ -32,7 +32,6 @@ const FooterDropdown = ({ items = [], dropdownTitle }) => {
     }
   };
 
-  // Función para manejar la selección de un elemento
   const handleItemClick = (item) => {
     setSelectedItem(item);
     setIsOpen(false);
