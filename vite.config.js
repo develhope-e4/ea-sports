50b0b42f36f3c39260c5fb1f1a4e57eea9a87330
Wilmer Salazar <wilmersalazarlara@gmail.com>
2023-10-23 16:24:22 +0200

    refactor: creacion de carpeta dist con vite build

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   // This changes the out put dir from dist to build change as your need
   // comment this out if that isn't relevant for your project
   build: {
-    outDir: 'build',
+    outDir: 'dist',
   },
   plugins: [
     reactRefresh(),
