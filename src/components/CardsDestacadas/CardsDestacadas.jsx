88902680312782ddb3bcfa67c11ee4aa54eb54ee
Nico Casiean <casiean2@gmail.com>
2023-11-05 13:51:44 +0100

    Subiendo cambios responsive a las cards destacadas, creando un componente para recogerer y reunir el bloque para poder panipularlo mejor, tambien he cambiado algunos detalles de otras seccioens para que la pagina tenga un width correcto

--- src/components/CardsDestacadas/CardsDestacadas.jsx
+++ src/components/CardsDestacadas/CardsDestacadas.jsx
@@ -1,19 +1,28 @@
 import CardDestacada from "../CardDestacada/CardDestacada";
-import classes from "./CardsDestacadas.module.scss";
+import "./CardsDestacadas.scss";
 import { cardDestacadaData } from "../../data/CardDestacadasData.mock";
 
 export const CardsDestacadas = () => {
-  return (
-    <div className={classes.CardsDestacadas}>
-      {cardDestacadaData &&
-        cardDestacadaData.map((card) => (
-          <CardDestacada
-            url="http://google.com"
-            backgroundImage={card.bg}
-            gameLogo={card.logo}
-            showText={true}
-          />
-        ))}
-    </div>
-  );
-};
+
+
+
+
+return (
+
+
+<div className="CardsDestacadas">
+
+          {cardDestacadaData &&
+            cardDestacadaData.map((card) => (
+              <CardDestacada
+                url="http://google.com"
+                backgroundImage={card.bg}
+                gameLogo={card.logo}
+                showText={true}
+              />
+            ))}
+
+</div>
+
+)
+}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -12,7 +12,6 @@ import Section from "../../components/Section/Section";
 import Container from "../../components/Container/Container";
 import { CardsDestacadas } from "../../components/CardsDestacadas/CardsDestacadas";
 
-
 const Home = () => {
   const [activeTab, setActiveTab] = useState("NoticiasEA");
 
@@ -38,17 +37,9 @@ const Home = () => {
       <Section>
         <h1>Juegos destacados</h1>
         <Container>
-          {cardDestacadaData &&
-            cardDestacadaData.map((card, index) => (
-              <CardDestacada
-                key={index}
-                url="http://google.com"
-                backgroundImage={card.bg}
-                gameLogo={card.logo}
-                className={index === 0}
-                showText={false}
-              />
-            ))}
+
+          <CardsDestacadas/>
+
         </Container>
         <Boton
           esPrimario={false}
