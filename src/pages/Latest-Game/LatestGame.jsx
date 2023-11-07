1e44304bfb9eeaa3817cde124856da4b24232684
Wilmer Salazar <wilmersalazarlara@gmail.com>
2023-11-07 17:03:54 +0100

    feat: modificando pagina Latest-Game

--- src/pages/Latest-Game/LatestGame.jsx
+++ src/pages/Latest-Game/LatestGame.jsx
@@ -1,14 +1,13 @@
 import { useState } from "react";
 import VideoComponent from "../../components/VideoComponent/VideoComponent";
-import { MdPausePresentation } from 'react-icons/md';
-import { BsPlayBtn } from 'react-icons/bs';
-import CardDestacada from "../../components/CardDestacada/CardDestacada";
-import { cardDestacadaData } from "../../data/CardDestacadasData.mock";
+import { MdPausePresentation } from "react-icons/md";
+import { BsPlayBtn } from "react-icons/bs";
 import Boton from "../../components/Boton/Boton";
 import BasePage from "../../components/BasePage/BasePage";
 import Section from "../../components/Section/Section";
 import Container from "../../components/Container/Container";
-import '../../components/VideoComponent/VideoComponent.module.scss';
+import "../../components/VideoComponent/VideoComponent.module.scss";
+import { CardsDestacadas } from "../../components/CardsDestacadas/CardsDestacadas";
 
 const LatestGame = () => {
   const [activeTab, setActiveTab] = useState("NoticiasEA");
@@ -29,40 +28,18 @@ const LatestGame = () => {
 
   return (
     <BasePage>
-      <div className="VideoContainer">
-        <VideoComponent
-          videoSrc="https://media.contentapi.ea.com/content/dam/eacom/videos/2023/10/ea-games-hero-md-fy24-sizzle-reel-7x2-xl.mp4"
-          webmSrcSmallest="https://media.contentapi.ea.com/content/dam/eacom/videos/2023/10/ea-games-hero-md-fy24-sizzle-reel-16x9-sm-xs.webm"
-          mp4SrcSmallest="https://media.contentapi.ea.com/content/dam/eacom/videos/2023/10/ea-games-hero-md-fy24-sizzle-reel-16x9-sm-xs.mp4"
-          webmSrcSmall="https://media.contentapi.ea.com/content/dam/eacom/videos/2023/10/ea-games-hero-md-fy24-sizzle-reel-16x9-sm-xs.webm"
-          mp4SrcSmall="https://media.contentapi.ea.com/content/dam/eacom/videos/2023/10/ea-games-hero-md-fy24-sizzle-reel-16x9-sm-xs.mp4"
-          webmSrcMedium="https://media.contentapi.ea.com/content/dam/eacom/videos/2023/10/ea-games-hero-md-fy24-sizzle-reel-2x1-md.webm"
-          mp4SrcMedium="https://media.contentapi.ea.com/content/dam/eacom/videos/2023/10/ea-games-hero-md-fy24-sizzle-reel-2x1-md.mp4"
-          webmSr="https://media.contentapi.ea.com/content/dam/eacom/videos/2023/10/ea-games-hero-md-fy24-sizzle-reel-5x2-lg.webm"
-          mp4Src="https://media.contentapi.ea.com/content/dam/eacom/videos/2023/10/ea-games-hero-md-fy24-sizzle-reel-5x2-lg.mp4"
-          webmSrcLargest="https://media.contentapi.ea.com/content/dam/eacom/videos/2023/10/ea-games-hero-md-fy24-sizzle-reel-7x2-xl.webm"
-          mp4SrcLargest="https://media.contentapi.ea.com/content/dam/eacom/videos/2023/10/ea-games-hero-md-fy24-sizzle-reel-7x2-xl.mp4"
-          autoplay={true}
-          loop={true}
-          muted={true}
-          PauseIcon={MdPausePresentation}
-          PlayIcon={BsPlayBtn}
-        />
-      </div>
+      <Section>
+        <Container>
+
+          
+        </Container>
+      </Section>
+        
+      
       <Section>
         <h1>Juegos destacados</h1>
         <Container>
-          {cardDestacadaData &&
-            cardDestacadaData.map((card, index) => (
-              <CardDestacada
-              key={index}
-                url="http://google.com"
-                ve
-                backgroundImage={card.bg}
-                gameLogo={card.logo}
-                showText={true}
-              />
-            ))}
+          <CardsDestacadas />
         </Container>
         <Boton
           esPrimario={false}
@@ -72,7 +49,9 @@ const LatestGame = () => {
         />
       </Section>
       <Section>
-        <Container></Container>
+        <Container>
+
+        </Container>
       </Section>
     </BasePage>
   );
