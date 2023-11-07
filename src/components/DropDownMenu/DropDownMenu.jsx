9e7e172d9a47217fa7773e42237ec695626a3be3
Nico Casiean <casiean2@gmail.com>
2023-11-07 03:42:03 +0100

    refactorizando navbar

--- src/components/DropDownMenu/DropDownMenu.jsx
+++ src/components/DropDownMenu/DropDownMenu.jsx
@@ -1,18 +1,29 @@
- 
 import "./DropDownMenu.scss";
-const DropDownMenu = ({ data, data2 }) => {
+const DropDownMenu = ({ data, data2, className }) => {
   return (
-    <div className="DropDownPadre">
-      <div className="DropDownMenu">
+    <div className="DropDownMenu">
+      <div className={className}>
+
+
+
         <ul>
-          {data2 && <h3>Explorar juegos</h3>}
+          {data2 && className === "first" && <h3>Explorar juegos</h3>}
           {data.map((data, index) => (
-            <li key={index}>
+            <div>            <li key={index}>
               <a href={data.url}>{data.text}</a>
+              
             </li>
+            <div className="separator"></div>
+            </div>
+
+            
           ))}
         </ul>
-        {data2 && (
+
+
+
+
+        {data2 && className === "first" && (
           <ul>
             <h3>Plataformas</h3>
             {data2.map((data, index) => (
@@ -20,6 +31,25 @@ const DropDownMenu = ({ data, data2 }) => {
                 <a href={data.url}>{data.text}</a>
               </li>
             ))}
+
+            {className === "second" && (
+
+
+
+
+              <div>
+                {data2.map((data, index) => (
+                  <li key={index}>
+                    <a href={data.url}>{data.text}</a>
+                  </li>
+
+
+
+
+
+                ))}
+              </div>
+            )}
           </ul>
         )}
       </div>
