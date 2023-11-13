27776bfb92a629ebd5f26d076ae79316832ec4c8
Jeniffer Flores <hachikofu03@gmail.com>
2023-11-13 15:02:58 +0100

    Feat originals page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Development from "./pages/development/Development";
 import Home from "./pages/home/Home";
 import NavBar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
+import Originals from "./pages/originals/Originals";
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route exact path="/dev" element={<Development />} />
+        <Route exact path="/dev" element={ <Development /> } />
+        <Route exact path="/ea-originals" element={ <Originals /> } />
       </Routes>
       <Footer />
     </BrowserRouter>
---
+++ src/pages/originals/Originals.jsx
@@ -0,0 +1,96 @@
+import LoopVideo from "../../components/LoopVideo/LoopVideo";
+import CardDestacada from "../../components/CardDestacada/CardDestacada";
+import { cardDestacadaData } from "../../data/CardDestacadasData.mock";
+import Tabs from "../../components/Tabs/Tabs";
+import cardData from "../../data/CardData.mock";
+import { useState } from "react";
+import Cards from "../../components/Cards/Cards";
+import Boton from "../../components/Boton/Boton";
+import Subscription from "../../components/Subscription/Subscription";
+import BasePage from "../../components/BasePage/BasePage";
+import Section from "../../components/Section/Section";
+import Container from "../../components/Container/Container";
+import { CardsDestacadas } from "../../components/CardsDestacadas/CardsDestacadas";
+
+
+const Home = () => {
+  const [activeTab, setActiveTab] = useState("NoticiasEA");
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
+
+  const tabNames = {
+    NoticiasEA: "Noticias De EA",
+    EASPORTSFC: "EA SPORTS FC",
+    StarWars: "Star Wars",
+    ApexLegends: "Apex Legends",
+    LosSims4: "Los Sims 4",
+    F1: "F1",
+    Battlefield: "Battlefield",
+  };
+
+  return (
+    <BasePage>
+      <div className="container">
+        <LoopVideo />
+      </div>
+      <Section>
+        <h1>Juegos destacados</h1>
+        <Container>
+
+          <CardsDestacadas/>
+
+
+        </Container>
+        <Boton
+          esPrimario={false}
+          isLight={false}
+          onClick={() => console.log("click en el boton negro")}
+          texto={"Últimos juegos"}
+        />
+      </Section>
+      <Section>
+        <h1>Últimas actualizaciones</h1>
+        <Container>
+          <div className="tabs-cards">
+            <Tabs
+              tabNames={tabNames}
+              handleTabClick={handleTabClick}
+              activeTab={activeTab}
+            />
+            <div className="card-container">
+              {cardData[activeTab] ? (
+                cardData[activeTab].map((card, index) => (
+                  <Cards
+                    key={`${index}-${card.nombre}`}
+                    imagenSrc={card.imagenSrc}
+                    nombre={card.nombre}
+                    fecha={card.fecha}
+                    titulo={card.titulo}
+                    texto={card.texto}
+                  />
+                ))
+              ) : (
+                <p></p>
+              )}
+            </div>
+          </div>
+        </Container>
+        <Boton
+          esPrimario={false}
+          isLight={false}
+          onClick={() => console.log("click en el boton negro")}
+          texto={"Leer mas"}
+        />
+      </Section>
+      <Section>
+        <Container>primer parte</Container>
+        <Container>Segunda</Container>
+      </Section>
+      <Subscription />
+    </BasePage>
+  );
+};
+
+export default Originals;
